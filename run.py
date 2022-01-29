bb738c320afc48c2c7c5e5bffe3ab52390dbedbd
mattteochen <matteochen3@gmail.com>
2022-01-29 10:08:34 -0800

    switched off vim plug installation (FIXME) and corrected directory make error

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
 import os
 import os.path
 
 header              = 'NVIM INSTALLATION: '
-dir_header          = 'DIR MAKE'
-vim_plug_header     = 'VIM PLUG EXTENSION'
-live_grep_header    = 'LIVE_GREP'
+dir_header          = 'DIR MAKE: '
+vim_plug_header     = 'VIM PLUG EXTENSION: '
+live_grep_header    = 'LIVE_GREP: '
 
 error               = 'ERROR'
 ok                  = 'OK'
@@ -20,19 +20,19 @@ if (ret != 0):
 else:
     print(f'{header}{ok}')
 
-ret = os.system('cd && cd ./config && mkdir nvim')
+ret = os.system('cd && cd .config/ && mkdir nvim')
 if (ret != 0):
-    print(f'{dir_header}{error}Please follow manual steps!')
+    print(f'{dir_header}{error}. Please follow manual steps!')
     exit(ret)
 else:
     print(f'{dir_header}{ok}')
 
-ret = os.system("sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'")
-if (ret != 0):
-    print(f'{vim_plug_header}{error}. Please follow manual steps!')
-    exit(ret)
-else:
-    print(f'{vim_plug_header}{ok}')
+#ret = os.system("sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'")
+#if (ret != 0):
+#    print(f'{vim_plug_header}{error}. Please follow manual steps!')
+#    exit(ret)
+#else:
+#    print(f'{vim_plug_header}{ok}')
 
 ret = os.system('sudo apt-get install ripgrep')
 if (ret != 0):
@@ -41,7 +41,7 @@ if (ret != 0):
 else:
     print(f'{live_grep_header}{ok}')
 
-ret = os.system('cp init.vim $HOME/.config/nvim/')
+ret = os.system('sudo cp init.vim $HOME/.config/nvim/')
 if (ret != 0):
     print('Can not move the nvim init file into its directory. Please move the config file in $HOME/.config/nvim .')
 else:
